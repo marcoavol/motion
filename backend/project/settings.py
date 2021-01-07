732dab50b3627322f8823056bb18c23eee84e271
M4V88 <marco_volken@bluewin.ch>
2021-01-07 13:48:09 +0100

    Adjustments to settings.py

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -126,12 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/static/'
 
 # Media files (user uploads, ...)
 # https://docs.djangoproject.com/en/3.1/topics/files/
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/media/'
 
 # Django rest_framework and simple_jwt authentication settings
 # https://django-rest-framework-simplejwt.readthedocs.io/en/latest/getting_started.html#usage
