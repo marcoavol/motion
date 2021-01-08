227a6d8fdb331b14a23944277ec189a2702d4599
M4V88 <marco_volken@bluewin.ch>
2021-01-08 13:22:27 +0100

    Update for changed hostname

--- frontend/src/store/constants.js
+++ frontend/src/store/constants.js
@@ -1,1 +1,2 @@
-export const baseURL = "https://motion.propulsion-home.ch/backend/api"
+export const baseURL = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8000/backend/api/' : 'http://mv-motion.propulsion-home.ch/backend/api/'
+
