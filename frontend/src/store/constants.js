8ae47b03b6d5809bb2439dcf25f95318d94cfe01
M4V88 <marco_volken@bluewin.ch>
2021-01-08 16:07:12 +0100

    Fix typo in baseURL

--- frontend/src/store/constants.js
+++ frontend/src/store/constants.js
@@ -1,2 +1,2 @@
-export const baseURL = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8000/backend/api' : 'https://mv-motion.propulsion-home.ch/backend/api'
+export const baseURL = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8000/backend/api' : 'https://mv-motion.propulsion-learn.ch/backend/api'
 
