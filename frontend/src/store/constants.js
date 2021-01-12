37b8790ed7ffa15c19eacf187df1900848336e7c
M4V88 <marco_volken@bluewin.ch>
2021-01-12 13:39:07 +0100

    Connect frontend with own backend

--- frontend/src/store/constants.js
+++ frontend/src/store/constants.js
@@ -1,4 +1,1 @@
 export const baseURL = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8000/backend/api' : 'https://mv-motion.propulsion-learn.ch/backend/api'
-
-// TODO: Delete below when not needed anymore!
-// export const baseURL = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8000/backend/api' : 'https://motion.propulsion-home.ch/backend/api'
