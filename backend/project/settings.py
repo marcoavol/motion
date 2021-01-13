22906f49a1649b6c2c507dfa8f910aab773da395
Marco Volken <marco_volken@bluewin.ch>
2021-01-13 08:35:40 +0000

    Update settings.py

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = ast.literal_eval(os.getenv('DJANGO_DEBUG'))
+DEBUG = True  # ast.literal_eval(os.getenv('DJANGO_DEBUG'))
 
 # SECURITY: restrict allowed hosts in production
 PRODUCTION_HOSTS = [
